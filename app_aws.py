1a48c7ce7f5c2bb5f5d77af0482603a9133631f2
Teja Sai Kalepalli <92510587+Tejasai37@users.noreply.github.com>
2026-01-28 19:38:44 +0530

    SNS updated
    SNS updated

--- app_aws.py
+++ app_aws.py
@@ -23,7 +23,7 @@ projects_table = dynamodb.Table('Projects')
 enrollments_table = dynamodb.Table('Enrollments')
 
 # SNS Topic ARN (Replace with your actual SNS Topic ARN)
-SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:604665149129:aws_capstone_topic' 
+SNS_TOPIC_ARN = 'arn:aws:sns:us-east-1:203918855127:project_topic' 
 
 # Configuration for File Uploads
 UPLOAD_FOLDER = 'static/uploads'
